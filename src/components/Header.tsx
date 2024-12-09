199fa2de30dd71f89df766d77d2791b032bdec87
SumairaaNaveed <naveedsumaira3@gmail.com>
2024-12-10 01:04:13 +0500

    done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
  import React from 'react';
 import { FaHeart, FaBell, FaSearch, FaCog } from 'react-icons/fa';
 import Image from 'next/image';
-import Link from 'next/link';
+
 
 const Header = () => {
   return (
